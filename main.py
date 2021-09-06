6a1d11e1df6fcca97f897f51b77fc467cc9b8635
Jarek Majka <vessekheal@gmail.com>
2021-09-06 22:45:47 +0200

    Messy calculation added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 0. Check if the number is greater than 16
+# 0. Check if the number is greater than 15
     # if True:
         # 1. Divide the decimal number by 16 using modulo operator
             # 1.1. Store the value in a list
@@ -7,6 +7,8 @@
     # if False:
         # 1. Store in a list
         # 2. Convert list elements into String and present the outcome
+from typing import Tuple
+
 
 def sanity_check(color):
     if color > 255:
@@ -17,7 +19,26 @@ def sanity_check(color):
         return color
 
 
+def rgb_input() -> Tuple[int, int, int]:
+    r_color = sanity_check(int(input('Please provide R color: ')))
+    g_color = sanity_check(int(input('Please provide G color: ')))
+    b_color = sanity_check(int(input('Please provide B color: ')))
+    return r_color, g_color, b_color
+
+
+def hex_list_builder(rgb_colors):
+    hex_container = '0123456789ABCDEF'
+    hex_list = []
+    # TODO Transform below code into recurrence function.
+    for color in rgb_colors:
+        if color > 15:
+            while color != 0:
+                hex_list.append(hex_container[color % 16])
+                color = color // 16
+        else:
+            hex_list.append(hex_container[color])
+    print(hex_list)
+
+
 if __name__ == '__main__':
-    r_color = int(input('Please provide R color: '))
-    g_color = int(input('Please provide G color: '))
-    b_color = int(input('Please provide B color: '))
+    hex_list_builder(rgb_input())
