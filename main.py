fa4623c9ffe8be9e82ac93fe4f88fe1f5d0b15e7
Jarek Majka <vessekheal@gmail.com>
2021-09-07 23:40:55 +0200

    RGB to hex solved

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-# 0. Check if the number is greater than 15
-    # if True:
-        # 1. Divide the decimal number by 16 using modulo operator
-            # 1.1. Store the value in a list
-        # 2. Divide the decimal again using floor division
-            # 2.1. Take the result and check against step 0
-    # if False:
-        # 1. Store in a list
-        # 2. Convert list elements into String and present the outcome
 from typing import Tuple
 
 
@@ -31,19 +22,14 @@ def hex_list_builder(rgb_colors):
     hex_list = []
     for color in rgb_colors:
         if color > 15:
-            # TODO Symbols are in wrong order.
-            while color != 0:
-                hex_list.append(hex_container[color % 16])
-                color = color // 16
-        elif color < 10:
-            hex_list.append('0' + hex_container[color])
+            hex_list.append(hex_container[(color // 16) % 16] + hex_container[color % 16])
         else:
-            hex_list.append(hex_container[color])
+            hex_list.append('0' + hex_container[color])
     return hex_list
 
 
 def output_formatter():
-    return ''.join([i for i in hex_list_builder(rgb_input())])
+    return '#' + ''.join([notation for notation in hex_list_builder(rgb_input())])
 
 
 if __name__ == '__main__':
